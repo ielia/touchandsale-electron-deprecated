f5b2aa205b73dcdb25720bbf8e2351af85675685
Ignacio Elia <elia.ignacio@gmail.com>
2022-03-09 01:32:33 -0300

    Added shortcut key representation to View Tab titles (i.e., tooltips).

--- src/Components/ViewTab.tsx
+++ src/Components/ViewTab.tsx
@@ -1,7 +1,7 @@
 import React, {PureComponent, RefObject, createRef} from 'react';
 
 import './ViewTab.scss';
-import {shortcutKeyToShortString} from '../commons';
+import {appendShortcutString, shortcutKeyToShortString} from '../commons';
 
 interface Props {
     actions: any; // TODO: See what to do with this.
@@ -72,8 +72,9 @@ export default class ViewTab extends PureComponent<Props> {
 
     render() {
         const {label, selected, shortcutKey, zIndex} = this.props;
+        const title = appendShortcutString(label, shortcutKey);
         return (
-            <div className={`view-tab${selected ? ' selected' : ''}`} tabIndex={selected ? 1 : -1} style={{zIndex: `${zIndex}`}} onClick={this.handleClick}
+            <div className={`view-tab${selected ? ' selected' : ''}`} tabIndex={selected ? 1 : -1} style={{zIndex: `${zIndex}`}} title={title} onClick={this.handleClick}
                  onKeyDown={this.handleKeyDown} ref={this.selfRef}>
                 <div className="outline">
                     <div className="label">{label}</div>
