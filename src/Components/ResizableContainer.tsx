8b74a5c7a4130a1cc5654a35e25ea398cfb24b39
Ignacio Elia <elia.ignacio@gmail.com>
2022-03-13 05:31:45 -0300

    First approach to shrinking when parent shrinks.

--- src/Components/ResizableContainer.tsx
+++ src/Components/ResizableContainer.tsx
@@ -25,12 +25,19 @@ function coord(value: number | string) {
 }
 
 export default class ResizableContainer extends Component<Props, State> {
+    containerRef: RefObject<HTMLDivElement>;
+    parentResizeObserver: ResizeObserver;
+
     constructor(props: Props) {
         super(props);
         this.state = {
             height: this.props.height ?? 100,
             width: this.props.width ?? 100,
         };
+        this.containerRef = createRef<HTMLDivElement>();
+        this.handleDrag = this.handleDrag.bind(this);
+        this.handleStartDrag = this.handleStartDrag.bind(this);
+        this.handleStopDrag = this.handleStopDrag.bind(this);
     }
 
     buildHandle(
@@ -57,27 +64,77 @@ export default class ResizableContainer extends Component<Props, State> {
         );
     }
 
-    handleDrag(containerRef: RefObject<HTMLDivElement>, xMultiplier: number, yMultiplier: number, event: DraggableEvent, data: DraggableData) {
-        const currentContainer = containerRef.current;
-        const height = parseInt(currentContainer.getAttribute('originalHeight'));
-        const width = parseInt(currentContainer.getAttribute('originalWidth'));
-        currentContainer.style.height = `${height + data.y * yMultiplier}px`;
-        currentContainer.style.width = `${width + data.x * xMultiplier}px`;
+    handleDrag(xMultiplier: number, yMultiplier: number, event: DraggableEvent, data: DraggableData) {
+        const currentContainer = this.containerRef.current;
+        const originalHeight = parseInt(currentContainer.getAttribute('originalHeight'));
+        const originalWidth = parseInt(currentContainer.getAttribute('originalWidth'));
+        const {height, width} = this.restrictSize(
+            currentContainer,
+            originalHeight + data.y * yMultiplier,
+            originalWidth + data.x * xMultiplier,
+            xMultiplier,
+            yMultiplier);
+        currentContainer.style.height = `${height}px`;
+        currentContainer.style.width = `${width}px`;
     }
 
-    handleStartDrag(containerRef: RefObject<HTMLDivElement>, event: DraggableEvent, data: DraggableData) {
-        const currentContainer = containerRef.current;
+    handleStartDrag(event: DraggableEvent, data: DraggableData) {
+        const currentContainer = this.containerRef.current;
         const computedStyle = getComputedStyle(currentContainer);
         currentContainer.setAttribute('originalHeight', computedStyle.height);
         currentContainer.setAttribute('originalWidth', computedStyle.width);
     }
 
-    handleStopDrag(containerRef: RefObject<HTMLDivElement>, event: DraggableEvent, data: DraggableData) {
-        const currentContainer = containerRef.current;
+    handleStopDrag(event: DraggableEvent, data: DraggableData) {
+        const currentContainer = this.containerRef.current;
         currentContainer.removeAttribute('originalHeight');
         currentContainer.removeAttribute('originalWidth');
         const computedStyle = getComputedStyle(currentContainer);
         this.setState(({height, width, ...other}) => ({height: parseInt(computedStyle.height), width: parseInt(computedStyle.width), ...other}));
+    }
+
+    restrictSize(currentContainer: HTMLElement, proposedHeight: number, proposedWidth: number, xMultiplier: number, yMultiplier: number): {height: number, width: number} {
+        let height: number = proposedHeight;
+        let width: number = proposedWidth;
+        const containerParent = currentContainer.parentElement;
+        if (containerParent) {
+            const containerRect = currentContainer.getBoundingClientRect();
+            const parentRect = containerParent.getBoundingClientRect();
+            if (xMultiplier < 0 && containerRect.right - width < parentRect.left) {
+                width = containerRect.right - parentRect.left;
+            } else if (xMultiplier > 0 && containerRect.left + width > parentRect.right) {
+                width = parentRect.right - containerRect.left;
+            }
+            if (yMultiplier < 0 && containerRect.bottom - height < parentRect.top) {
+                height = containerRect.bottom - parentRect.top;
+            } else if (yMultiplier > 0 && containerRect.top + height > parentRect.bottom) {
+                height = parentRect.bottom - containerRect.top;
+            }
+        }
+        return {height, width};
+    }
+
+    componentDidMount() {
+        // TODO: Figure out edges
+        const parentElement = this.containerRef.current.parentElement;
+        this.parentResizeObserver = new ResizeObserver(entries => {
+            const edges = this.props.resizableEdges;
+            const currentContainer = this.containerRef.current;
+            const containerRect = currentContainer.getBoundingClientRect();
+            const {height, width} = this.restrictSize(currentContainer, containerRect.height, containerRect.width, 1, 1);
+            if (height !== containerRect.height) {
+                currentContainer.style.height = `${height}px`;
+            }
+            if (width !== containerRect.width) {
+                currentContainer.style.width = `${width}px`;
+            }
+        });
+        this.parentResizeObserver.observe(parentElement);
+    }
+
+    componentWillUnmount() {
+        this.parentResizeObserver.disconnect();
+        this.parentResizeObserver = null;
     }
 
     render() {
@@ -96,38 +153,34 @@ export default class ResizableContainer extends Component<Props, State> {
             {}
         );
 
-        const containerRef = createRef<HTMLDivElement>();
-        const handleDrag = this.handleDrag.bind(this, containerRef);
-        const handleStartDrag = this.handleStartDrag.bind(this, containerRef);
-        const handleStopDrag = this.handleStopDrag.bind(this, containerRef);
         return (
             <div className={`resizable-container ${className ?? ''} ${resizableEdges.map(edge => getCompassOctoHeadingClassNameString(edge)).join(' ')}`}
                  style={{bottom: coord(bottom), height: coord(height), left: coord(left), right: coord(right), top: coord(top), width: coord(width)}}
-                 ref={containerRef}>
+                 ref={this.containerRef}>
                 <div className="horizontal-arrangement top">
-                    {edgesAndCorners['nw'] ? this.buildHandle('top-top-left', 'both', handleStartDrag, handleDrag.bind(this, -1, -1), handleStopDrag) : null}
-                    {edgesAndCorners['n'] ? this.buildHandle('top', 'y', handleStartDrag, handleDrag.bind(this, 0, -1), handleStopDrag) : null}
-                    {edgesAndCorners['ne'] ? this.buildHandle('top-top-right', 'both', handleStartDrag, handleDrag.bind(this, 1, -1), handleStopDrag) : null}
+                    {edgesAndCorners['nw'] ? this.buildHandle('top-top-left', 'both', this.handleStartDrag, this.handleDrag.bind(this, -1, -1), this.handleStopDrag) : null}
+                    {edgesAndCorners['n'] ? this.buildHandle('top', 'y', this.handleStartDrag, this.handleDrag.bind(this, 0, -1), this.handleStopDrag) : null}
+                    {edgesAndCorners['ne'] ? this.buildHandle('top-top-right', 'both', this.handleStartDrag, this.handleDrag.bind(this, 1, -1), this.handleStopDrag) : null}
                 </div>
                 <div className="horizontal-arrangement inline">
                     <div className="vertical-arrangement left">
-                        {edgesAndCorners['nw'] ? this.buildHandle('left-top-left', 'both', handleStartDrag, handleDrag.bind(this, -1, -1), handleStopDrag) : null}
-                        {edgesAndCorners['w'] ? this.buildHandle('left', 'x', handleStartDrag, handleDrag.bind(this, -1, 0), handleStopDrag) : null}
-                        {edgesAndCorners['sw'] ? this.buildHandle('left-bottom-left', 'both', handleStartDrag, handleDrag.bind(this, -1, -1), handleStopDrag) : null}
+                        {edgesAndCorners['nw'] ? this.buildHandle('left-top-left', 'both', this.handleStartDrag, this.handleDrag.bind(this, -1, -1), this.handleStopDrag) : null}
+                        {edgesAndCorners['w'] ? this.buildHandle('left', 'x', this.handleStartDrag, this.handleDrag.bind(this, -1, 0), this.handleStopDrag) : null}
+                        {edgesAndCorners['sw'] ? this.buildHandle('left-bottom-left', 'both', this.handleStartDrag, this.handleDrag.bind(this, -1, -1), this.handleStopDrag) : null}
                     </div>
                     <div className="resizable-container-content">
                         {children}
                     </div>
                     <div className="vertical-arrangement right">
-                        {edgesAndCorners['ne'] ? this.buildHandle('right-top-right', 'both', handleStartDrag, handleDrag.bind(this, 1, 1), handleStopDrag) : null}
-                        {edgesAndCorners['e'] ? this.buildHandle('right', 'x', handleStartDrag, handleDrag.bind(this, 1, 0), handleStopDrag) : null}
-                        {edgesAndCorners['se'] ? this.buildHandle('right-bottom-right', 'both', handleStartDrag, handleDrag.bind(this, 1, 1), handleStopDrag) : null}
+                        {edgesAndCorners['ne'] ? this.buildHandle('right-top-right', 'both', this.handleStartDrag, this.handleDrag.bind(this, 1, 1), this.handleStopDrag) : null}
+                        {edgesAndCorners['e'] ? this.buildHandle('right', 'x', this.handleStartDrag, this.handleDrag.bind(this, 1, 0), this.handleStopDrag) : null}
+                        {edgesAndCorners['se'] ? this.buildHandle('right-bottom-right', 'both', this.handleStartDrag, this.handleDrag.bind(this, 1, 1), this.handleStopDrag) : null}
                     </div>
                 </div>
                 <div className="horizontal-arrangement bottom">
-                    {edgesAndCorners['sw'] ? this.buildHandle('bottom-bottom-left', 'both', handleStartDrag, handleDrag.bind(this, -1, 1), handleStopDrag) : null}
-                    {edgesAndCorners['s'] ? this.buildHandle('bottom', 'y', handleStartDrag, handleDrag.bind(this, 0, 1), handleStopDrag) : null}
-                    {edgesAndCorners['se'] ? this.buildHandle('bottom-bottom-right', 'both', handleStartDrag, handleDrag.bind(this, 1, 1), handleStopDrag) : null}
+                    {edgesAndCorners['sw'] ? this.buildHandle('bottom-bottom-left', 'both', this.handleStartDrag, this.handleDrag.bind(this, -1, 1), this.handleStopDrag) : null}
+                    {edgesAndCorners['s'] ? this.buildHandle('bottom', 'y', this.handleStartDrag, this.handleDrag.bind(this, 0, 1), this.handleStopDrag) : null}
+                    {edgesAndCorners['se'] ? this.buildHandle('bottom-bottom-right', 'both', this.handleStartDrag, this.handleDrag.bind(this, 1, 1), this.handleStopDrag) : null}
                 </div>
             </div>
         );
